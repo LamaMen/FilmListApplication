d7efbcb5a9c723ff833853b243c3172ea6dc3159
ilia (linux) <ilia.rodionov20021801@gmail.com>
2020-09-05 18:55:08 +0300

    Добален NavigationDrawer и проект переведен на AppCompat

--- app/src/main/java/com/test/films_list_application/MainActivity.java
+++ app/src/main/java/com/test/films_list_application/MainActivity.java
@@ -2,7 +2,9 @@ package com.test.films_list_application;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,8 +12,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.MenuItemCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.test.films_list_application.dao.Films;
 import com.test.films_list_application.models.Film;
 
@@ -20,7 +28,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     public final static Map<Integer, Film> mapFilms = new HashMap<>();
 
     private final static String TAG = MainActivity.class.toString();
@@ -33,6 +42,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getHeaderView(0).setBackgroundColor(Color.RED);
+
+        MenuItem menuItem = navigationView.getMenu().findItem(R.id.nav_gallery2);
+        TextView acView = (TextView) MenuItemCompat.getActionView(menuItem);
+        acView.setGravity(Gravity.CENTER_VERTICAL);
+        acView.setTypeface(null, Typeface.BOLD_ITALIC);
+        acView.setText("99");
+        acView.setTextColor(Color.RED);
+        int a = 0;
+
 
         if (savedInstanceState != null) {
             changedColorTexts = savedInstanceState.getIntegerArrayList(KEY_TEXT);
@@ -49,6 +81,41 @@ public class MainActivity extends AppCompatActivity {
         for (Film film : films) {
             mapFilms.put(film.getId(), film);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
