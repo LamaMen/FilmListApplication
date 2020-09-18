75235a53d4ac3663b76097c246a70a169ca4a577
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2020-09-18 21:06:26 +0300

    Создан RecyclerView и добавлен на главный экран

--- app/src/main/java/com/test/films_list_application/activities/FilmItemViewHolder.java
+++ app/src/main/java/com/test/films_list_application/activities/FilmItemViewHolder.java
@@ -1,5 +1,6 @@
 package com.test.films_list_application.activities;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,21 +8,30 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
 import com.test.films_list_application.R;
+import com.test.films_list_application.activities.fragments.ListFilmsFragment;
 import com.test.films_list_application.dao.models.Film;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FilmItemViewHolder extends RecyclerView.ViewHolder {
-    private TextView filmName;
-    private ImageView filmCover;
+    @BindView(R.id.film_name)
+    public TextView filmName;
+    @BindView(R.id.film_cover)
+    public ImageView filmCover;
+    @BindView(R.id.film_show_details)
+    public MaterialButton filmShowDetailsButton;
 
     public FilmItemViewHolder(@NonNull View itemView) {
         super(itemView);
-        filmName = itemView.findViewById(R.id.film_name);
-        filmCover = itemView.findViewById(R.id.film_cover);
+        ButterKnife.bind(this, itemView);
     }
 
     public void bind(Film film) {
         filmName.setText(film.getName());
         filmCover.setImageResource(film.getPhotoID());
+        filmShowDetailsButton.setOnClickListener(v -> Log.d(ListFilmsFragment.TAG, "Button on item " + film.getId() + " pressed"));
     }
 }
